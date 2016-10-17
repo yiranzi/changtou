d656b0eb709add72bda0c26e91eecc7328878673
yunyiGG <zl0542@foxmail.com>
2016-10-17 17:11:22 +0800

    增加浏览器微信判断

--- src/plugin/Agent.js
+++ src/plugin/Agent.js
@@ -4,3 +4,28 @@
  * 浏览器相关
  */
 
+/**
+ * 判断是否为微信内置浏览器
+ * @returns {boolean}
+ */
+const getIsWx = () => {
+  var ua = window.navigator.userAgent.toLowerCase()
+  if (ua.match(/MicroMessenger/i) && ua.match(/MicroMessenger/i)[0] === 'micromessenger') {
+    return true
+  } else {
+    return false
+  }
+}
+
+/**
+ *
+ */
+export default {
+  get isWx () {
+    return getIsWx()
+  },
+
+  get userAgent () {
+    return window.navigator.userAgent
+  }
+}
