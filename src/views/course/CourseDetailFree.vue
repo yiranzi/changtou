1c6481b701fc0c5436103c793b640b25c72f6ba1
ExclusiveRose <747834137@qq.com>
2016-12-06 17:10:16 +0800

    2016/12/6 多个bug修改

--- src/views/course/CourseDetailFree.vue
+++ src/views/course/CourseDetailFree.vue
@@ -363,6 +363,7 @@
        * 参加课程
        */
       join () {
+        console.log(this.isUserLogin)
         if (this.isUserLogin) {
           const me = this
           me.joinSubject(parseInt(me.subjectId)).then(
