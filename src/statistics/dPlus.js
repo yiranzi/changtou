f9d314d532677a49e5ba2a864af32963785965d7
jun <87801036@qq.com>
2016-12-13 13:47:41 +0800

    2016.12.13 数据统计

---
+++ src/statistics/dPlus.js
@@ -0,0 +1,152 @@
+/**
+ * Created by jun on 2016/12/13.
+ * Dplus 数据统计
+ */
+import {D_PLUS_ID} from '../frame/serverConfig'
+
+/**
+ * 是否支持dplus统计
+ * @returns {boolean}
+ */
+const isSupport = () => {
+  return !!window.dplus
+}
+
+/**
+ * 初始化dplus配置
+ */
+const init = () => {
+  if (isSupport()) {
+    window.dplus.init(D_PLUS_ID, {
+      //"disable_cookie": true,
+      //"cross_subdomain_cookie": true,
+      localstorage: true,
+      track_timeout: 1000, //回调响应时间
+
+      loaded: function(){
+        //todo EventBus.fireEvent(eventMap.DPLUS_READY)
+      }
+    })
+  }
+}
+
+/**
+ *  停止制定事件的统计
+ *  @params events []  事件的数组
+ *  若是没有参数  表示停止所有的事件统计
+ *
+ */
+const disable = (events) => {
+  if(events){
+    window.dplus.disable(events)
+  }else{
+    window.dplus.disable()
+  }
+}
+
+/**
+ * 发送一次事件
+ * @param eventName string  事件名称
+ * @param properties object 事件属性
+ * @param callback
+ */
+const track = (eventName, properties, callback) => {
+  if (isSupport()) {
+    window.dplus.track(eventName, properties || {}, function(){
+      if (callback) {
+        callback()
+      } else {
+        //console.info('发送统计数据回调',' 事件：'+ eventname);
+      }
+    })
+  }
+}
+
+/**
+ * 注册超级属性，后面发送的事件会自动带上此类标签
+ * @param properties {} object 类型
+ *
+ * 使用示例：
+ *  register（{name：zl, sex: male}）
+ */
+const register = (properties) => {
+  if (isSupport()) {
+    window.dplus.register(properties)
+  }
+
+  if (window.window.ictData) {
+    window.ictData.register(properties)
+  }
+}
+
+/**
+ *  删除执行的超级属性
+ * @param properties String/Array  需要删除的属性值
+ */
+const unregister = (properties) => {
+  if (isSupport()) {
+    window.dplus.unregister(properties)
+  }
+}
+
+/**
+ *  返回指定的超级属性名称
+ *  @params propertyName 属性名称
+ */
+const getProperty = (propertyName) => {
+  if(isSupport()){
+    return window.dplus.get_property(propertyName)
+  }else{
+    return null
+  }
+}
+
+/**
+ * 删除所有用户标识， 超级属性
+ * 调用此方法后，再次调用 track 方法，会认为是一个新用户
+ */
+const clear = () => {
+  if(isSupport()){
+    window.dplus.clear()
+  }
+}
+
+/**
+ * （动态）设置配置信息
+ *  @param config
+ *
+ *  例如：
+ *  setConfig（{track_timeout: 500}）
+ */
+const setConfig = (config) => {
+  if(isSupport()){
+    window.dplus.set_config(config)
+  }else{
+    console.warn('dplus不存在，你尝试设置属性', config)
+  }
+}
+
+/**
+ * 获取指定的config配置
+ * @param config
+ */
+const getConfig = (config) => {
+  if(isSupport()){
+    return window.dplus.get_config(config)
+  }else{
+    console.warn('dplus不存在，你尝试获取属性', config)
+    return null
+  }
+}
+
+export const dPlus = {
+  init,
+  disable,
+  track,
+  register,
+  unregister,
+  getProperty,
+  clear,
+  setConfig,
+  getConfig
+}
---
+++ src/statistics/statisticsEventsFactory.js
@@ -0,0 +1,321 @@
+/**
+ * Created by jun on 2016/12/13.
+ */
+import dPlus from './dPlus'
+import ictData from './ictData'
+import {Device, platformMap} from '../plugin/device'
+
+/**
+ * 统计事件
+ * @type {{}}
+ */
+const events = {
+  APP_START: '启动',
+
+  /**
+   *
+   */
+  PAGE_TIME: '页面停留时长',
+
+  /**
+   *
+   */
+  WX_SHARE: '微信内分享',
+  WX_SHARE_SUCCESS: '微信内分享成功',
+  WX_SHARE_CANCEL: '微信内分享取消',
+
+  /**
+   *
+   */
+  TAP_FT_ANDROID_LOAD: '点击富途安卓下载',
+  TAP_FT_IOS_LOAD: '点击富途iOS下载',
+  TAP_FT_OPEN: '富途页打开数',
+  TAP_FT_COURSE: '富途长投课程点击数',
+  TAP_FT_SECTION: '富途港股打新课小节点击数',
+  TAP_FT_OAUTH_SUCCESS: '富途授权成功',
+
+  /**
+   * 课程
+   */
+  SUBJECT_JOIN_TAP: '点击“参加课程”按钮',
+  SUBJECT_BUY_NOW_TAP: '课程-点击“立即购买”按钮',
+  SUBJECT_AUDITION_TAP: '课程-点击“试听”按钮',
+  /**
+   * 专题
+   */
+  TOPIC_CONFIRM_TAP: '专题-点击“立即支付”按钮',
+  TOPIC_SUBJECT_TAP: '专题-点击课程',
+  TICKET_CONFIRM_TAP: '年会-点击“立即支付”按钮',
+
+  /**
+   * 支付流程
+   */
+  ORDER_CONFIRM_TAP: '点击“确认订单”按钮',
+  PAY_CONFIRM_TAP: '点击“确认支付”按钮',
+  PAY_SUCCESSFUL: '支付成功',
+  PAY_FAIL: '支付失败',
+  PAY_CANCEL: '支付取消',
+
+  /**
+   * 首页
+   */
+  HOME_PIC_TAP: '点击首页图片',
+  HOME_TEXT_TAP: '点击首页文字',
+
+  LOGIN_NOW: '点击立即登录',
+
+  /**
+   * 作业
+   */
+  DO_HOMEWORK: '点击"写作业"',
+  SUBMIT_HOMEWORK: '点击"提交作业"',
+  SAVE_DRAFT: '点击"保存草稿"',
+
+  CHOICE_QUESTION_BEGIN: '选择题-点击"开始测试"',
+  CHOICE_QUESTION: '选择题-答题',
+  CHOICE_QUESTION_RETEST: '选择题-重测',
+
+  /**
+   * 每日一题
+   */
+  DAILY_QUIZ: '每日一题',
+
+  /**
+   * 院生访谈
+   */
+  INTERVIEW: '院生访谈-点击',
+  INTERVIEW_SHARE_TAP: '院生访谈-点击分享',
+
+  /**
+   * 登录注册流程
+   */
+  REGISTER_INPUT_IDENTITY: '注册-输入-账号',
+  REGISTER_INPUT_PASSWORD: '注册-输入-密码',
+  REGISTER_INPUT_VALIDATION_CODE: '注册-输入-验证码',
+
+  REGISTER_TAP_REGISTER: '注册-点击-注册',
+  REGISTER_TAP_SUBMIT: '注册-点击-提交',
+
+  REGISTER_VALIDATION_CODE_BACK: '注册-验证码页面-返回',
+  REGISTER_IDENTITY_BACK: '注册-手机号页面-返回',
+
+  BIND_PHONE_INPUT_PHONE: '绑定手机-输入-手机号',
+  BIND_PHONE_INPUT_VALIDATION_CODE: '绑定手机-输入-验证码',
+  BIND_PHONE_TAP_NEXT: '绑定手机-点击-下一步',
+  BIND_PHONE_TAP_SUBMIT: '绑定手机-点击-提交',
+
+  BIND_PHONE_PHONE_BACK: '绑定手机-手机号页面-返回',
+  BIND_PHONE_VALIDATION_CODE_BACK: '绑定手机-验证码页面-返回',
+
+  LOGIN_BIND_PHONE_TAP_CANCEL: '登录-绑定手机-点击-取消',
+  LOGIN_BIND_PHONE_TAP_BIND: '登录-绑定手机-点击-绑定',
+
+  KNOWLEDGE_ENTRY: '知识地图-点击进入',
+  KNOWLEDGE_ARTICLE: '知识地图-3级知识点浮层',
+
+  NEWER_TEST_SUBJECT_TAP: '理财揭秘-点击课程',
+
+  ADVER_MY_COURSE_NO_LOGIN_KNOWLEDGE: '我的课程-未登录-点击广告A',
+  ADVER_MY_COURSE_LOGIN_KNOWLEDGE: '我的课程-已登录-点击广告B',
+  ADVER_MY_COURSE_LOGIN_KNOWLEDGE_CLOSE: '我的课程-已登录-删除广告'
+}
+
+/**
+ * 初始化配置
+ */
+const initConfig = () => {
+  var user = User.get()
+  //设置平台
+  dPlus.register({
+    '平台': Device.platform()
+  })
+  dPlus.register({
+    '长投Id': (user && user.userId) || '00'
+  })
+
+  //设置长投统计数据
+  ictData.register({
+    'platform': Device.platform()
+  })
+  ictData.register({
+    'userId': (user && user.userId) || '00'
+  })
+}
+
+/**
+ *  绑定监听的事件
+ */
+const bindEvents = () => {
+  /**
+   * 通用事件发生
+   */
+  EventBus.on(eventMap.EVENT_TRACK, this.event_track, this)
+
+  /**
+   * 登陆事件
+   * 注册事件
+   * 设置用户的超级属性，并且上传一次登陆事件
+   */
+  EventBus.on(eventMap.LOGIN_SUCCESS,this.stat_login,this)
+  EventBus.on(eventMap.REGISTER_SUCCESS,this.stat_register,this)
+  EventBus.on(eventMap.LOGOUT_SUCCESS,this.stat_logout,this)
+
+  /**
+   * 页面跳转
+   */
+  EventBus.on(eventMap.VIEW_CHANGE,this.stat_view_change,this)
+
+  /**
+   * 微信分享事件
+   */
+  EventBus.on(eventMap.WX_SHARE,this.stat_share,this)
+  EventBus.on(eventMap.WX_SHARE_SUCCESS,this.stat_share_success,this)
+  EventBus.on(eventMap.WX_SHARE_CANCEL,this.stat_share_cancel,this)
+}
+
+/**
+ * 设置用户
+ * @param user {obj/null}
+ * @param type string {login/register/null}
+ */
+const setUser = (userconfig, type) => {
+  var me = this
+  dplus.define('user', function (user) {
+    user.setId(userconfig ? userconfig.userId : '00')
+    user.setName(userconfig ? userconfig.userName: '游客')
+    user.setPhone(userconfig ? (userconfig.phone || '未知') : '未知')
+    user.setAccountType(userconfig ? (userconfig.accountType || '未知'): '未知')
+
+    if(type && type === me.user_type.LOGIN){
+      user.login()
+    }else if(type && type === me.user_type.REGISTER){
+      user.signUp()
+    }
+  })
+}
+
+/**
+ * 统计事件
+ * @param eventName 事件名称
+ * @param props 属性值
+ */
+const eventTrack = (eventName, props) =>{
+  dPlus.track(eventName, props)
+  ictData.track(eventName, props)
+}
+
+/**
+ * 统计登陆事件
+ * 统计登陆，并设计超级属性
+ */
+const statLogin = (user) => {
+  dPlus.register({
+    '长投Id': (user && user.userId) || '00'
+  })
+  this.setUser(user, this.user_type.LOGIN)
+
+  ictData.register({
+    userId: user.userId
+  })
+  ictData.updateUser(user)
+  ictData.track('登录')
+}
+
+/**
+ * 统计注册事件
+ * 统计注册，并设计超级属性
+ */
+const statRegister = (user) => {
+  dPlus.register({
+    '长投Id': (user && user.userId) || '00'
+  })
+  this.setUser(user, this.user_type.REGISTER)
+
+  ictData.register({
+    userId: user.userId
+  })
+  ictData.updateUser(user)
+  ictData.track('注册')
+}
+
+/**
+ * 登出事件
+ * 重置
+ */
+const statLogout = () => {
+  dPlus.track('退出账号')
+  this.setUser()
+  dPlus.register({
+    '长投Id':  '00'
+  })
+
+  ictData.register({
+    userId: '00'
+  })
+}
+
+preView: null
+
+/**
+ * 页面跳转
+ */
+const viewChange = (view) => {
+  //console.log('stat_view_change',view.config.stConfig,dPlus.isSupport())
+  if(preView){
+    var time = ((Date.now() - preView.startTime)/1000).toFixed(0)
+    dPlus.track(steventMap.PAGE_TIME,{'时长': time, '页面':preView.url})
+
+    ictData.track(steventMap.PAGE_TIME,{'时长': time, '页面':preView.url})
+  }
+
+  preView = {
+    startTime: Date.now(),
+    url: location.hash
+  }
+
+  dPlus.isSupport() && dplus.define('page', function (page) {
+    page.setType(view.config.stConfig && view.config.stConfig.type || '未知')
+    page.setTitle(view.config.stConfig && view.config.stConfig.title || '未知')
+    page.setTags(view.config.stConfig && view.config.stConfig.tags || ['未知'])
+    page.setCategory(view.config.stConfig && view.config.stConfig.categories || ['未知'])
+    page.setAuthor(view.config.stConfig && view.config.stConfig.author || 'dev')
+    page.setEditor(view.config.stConfig && view.config.stConfig.editor || 'dev')
+    page.view({
+      '访问页面': location.hash
+    })
+  })
+
+  ictData.track('页面访问',{ '页面':location.hash})
+}
+
+/**
+ * 统计分享事件
+ * @param props 事件属性
+ */
+const share = (props) =>{
+  //console.log('stat_share',this.events.WX_SHARE,props)
+  dPlus.track(this.events.WX_SHARE,props)
+
+  ictData.track(this.events.WX_SHARE,props)
+}
+
+/**
+ * 统计分享成功事件
+ * @param props 事件属性
+ */
+const shareSuccess = (props) => {
+  //console.log('stat_share_success',this.events.WX_SHARE,props)
+  dPlus.track(this.events.WX_SHARE_SUCCESS,props)
+
+  ictData.track(this.events.WX_SHARE_SUCCESS,props)
+}
+
+/**
+ * 统计分享取消事件
+ * @param props 事件属性
+ */
+const stat_share_cancel = (props) => {
+  dPlus.track(this.events.WX_SHARE_CANCEL,props)
+
+  ictData.track(this.events.WX_SHARE_CANCEL,props)
+}
