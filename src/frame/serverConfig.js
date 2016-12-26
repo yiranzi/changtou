964b9a9b9aedae561fe2d5fb86bc0bf16ed0094f
yunyiGG <zl0542@foxmail.com>
2016-12-26 16:04:17 +0800

    ictserver的配置

--- src/frame/serverConfig.js
+++ src/frame/serverConfig.js
@@ -7,7 +7,7 @@ import {debug} from '../util/environment'
 /**
  * 开发 + 测试
  */
-const SEVER_URL = debug ? 'http://dev.ichangtou.com.cn' : 'http://app.ichangtou.com.cn'
+const SEVER_URL = debug ? 'http://app.ichangtou.com.cn' : 'http://app.ichangtou.com.cn'
 
 const API_TOKEN = debug ? 'XX:_:w2qlJFV@ccOeiq41ENp><ETXh3o@aX8M<[_QOsZ<d8[Yz:NIMcKwpjtBk0e' : 'XX:_:w2qlJFV@ccOeiq41ENp><ETXh3o@aX8M<[_QOsZ<d8[Yz:NIMcKwpjtBk0e'
 
@@ -15,25 +15,30 @@ const CONTENT_TYPE = 'application/json; charset=utf-8'
 
 const D_PLUS_ID = debug ? 'da168f4458633q92c79b' : 'da168f4458633q92c79b'
 
-const WX_APPID = debug ? 'wx7cf8dd5d80048e42' : 'wxd6c823882698f217'
+const WX_APPID = debug ? 'wx7cf8dd5d80048e42' : 'http://121.40.131.112:3000'
+
+const ICT_SERVER = debug ? 'http://121.40.131.112:3000' : 'http://121.40.131.112:3000'
 
 /**
  * 开发 + 上线
  */
 //const SEVER_URL = debug ? 'http://dev.ichangtou.com.cn' : 'http://m.ichangtou.net'
-
-//const API_TOKEN = debug ? 'XX:_:w2qlJFV@ccOeiq41ENp><ETXh3o@aX8M<[_QOsZ<d8[Yz:NIMcKwpjtBk0e' : 'XX:_:w2qlJFV@ccOeiq41ENp><ETXh3o@aX8M<[_QOsZ<d8[Yz:NIMcKwpjtBk0e'
-
+//
+//const API_TOKEN = debug ? 'XX:_:w2qlJFV@ccOeiq41ENp><ETXh3o@aX8M<[_QOsZ<d8[Yz:NIMcKwpjtBk0e' : 'DE:_:w2qlJFV@ccOeiq41ENp><ETXh3o@aX8M<[_QOsZ<d8[Yz:NIMcKwpjtBk0e'
+//
 //const CONTENT_TYPE = 'application/json; charset=utf-8'
-
+//
 //const D_PLUS_ID = debug ? 'da168f4458633q92c79b' : 'ea16565d66171ebe26f4'
-
+//
 //const WX_APPID = debug ? 'wx7cf8dd5d80048e42' : 'wxd6c823882698f217'
+//
+//const ICT_SERVER = debug ? 'http://121.40.131.112:3000' : 'http://120.55.125.37:3000'
 
 export {
   SEVER_URL,
   API_TOKEN,
   CONTENT_TYPE,
   D_PLUS_ID,
-  WX_APPID
+  WX_APPID,
+  ICT_SERVER
 }
