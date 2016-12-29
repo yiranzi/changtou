8e8f2abb3f5efe16c32ac0084890743e9ae4d18f
jun <87801036@qq.com>
2016-12-29 10:29:20 +0800

    2016.12.29 分享ui调整

--- src/views/interview/InterviewRecord.vue
+++ src/views/interview/InterviewRecord.vue
@@ -136,8 +136,8 @@
       }
       .wechat,.timeline{
         display: inline-block;
-        width: 2rem;
-        height: 2rem;
+        width: 2.5rem;
+        height: 2.5rem;
       }
       .wechat{
         background: url("../../../static/image/interview/share-wechat.png") no-repeat center center / 100%;
