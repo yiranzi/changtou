459dad43f983ddeb721ea7185f03a892efdf35ae
zhongyan <zhongyan@ichangtou.con>
2017-03-02 10:43:38 +0800

    合并actions apiurl

--- src/frame/apiConfig.js
+++ src/frame/apiConfig.js
@@ -210,7 +210,7 @@ const apiUrl = {
   'village_get_answer_record': '/eden/answer-record',  // 获得答题进度
   'village_update_answer_record': '/eden/answer-record/:chapterNo/:questionNo', //提交更新答题进度
   'village_get_proposal_list': '/eden/proposal-list/:pageIndex/:pageSize',  //获得新手村吐槽列表
-  'village_submit_proposal_list': ' /eden/proposal-submit' //提交新手村吐槽
+  'village_submit_proposal_list': '/eden/proposal-submit' //提交新手村吐槽
 }
 
 export const getUrl = (urlPath, server = SEVER_URL, urlMap = apiUrl) => server + urlMap[urlPath]
--- src/vuex/freshVillage/actions.js
+++ src/vuex/freshVillage/actions.js
@@ -4,6 +4,36 @@
 import {getWithoutAuth, postWithinAuth} from '../../frame/ajax'
 import {getUrl} from '../../frame/apiConfig'
 
+//新手村全部资料
+const material = [
+  {
+    chapterNo: 1, //章节序号
+    title: '', //章节标题
+    story: '', // 章节故事
+    task: '',  //章节任务
+    questionArr: [
+      {
+        questionText: '',  // 题目问题
+        options: [],     // 题目选项
+        answer: '',     // 题目最佳答案
+        feedback: '',   // 答题反馈
+        extendsMaterialType: '', // 其他扩展资料类型
+        proverbs: '',      // 投资箴言
+        emergency: {      // 突发事件
+          story: '',        // 故事
+          score: ''        // 被扣掉的生命值
+        },
+        wisdom: {
+          title: '',    //今日小智标题
+          content: ''   // 今日小智内容
+        }
+      }
+    ]
+  },
+  {
+
+  }
+]
 /**
  * 获取吐槽内容列表
  * @param dispatch
@@ -15,7 +45,7 @@ export const getAdvise = ({ dispatch }, index, size) => {
   return new Promise(
     (resolve, reject) => {
       getWithoutAuth({
-        url: getUrl('get_advise').replace(':index', index).replace(':size', size)
+        url: getUrl('village_get_proposal_list').replace(':pageIndex', index).replace(':pageSize', size)
       }).then(
         function (advise) {
           dispatch('LOAD_VILLAGE_ADVISE', advise)
@@ -39,7 +69,7 @@ export const submitAdvise = ({ dispatch }, advise) => {
   return new Promise(
     (resolve, reject) => {
       postWithinAuth({
-        url: getUrl('submit_advise'),
+        url: getUrl('village_submit_proposal_list'),
         data: advise
       }).then(
         function () {
