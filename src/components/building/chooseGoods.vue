2968fc1785cdf9a450d038a512c4efd828220875
sunyiran <570932296@qq.com>
2017-03-16 19:12:31 +0800

    选择物品

--- src/components/building/chooseGoods.vue
+++ src/components/building/chooseGoods.vue
@@ -1,14 +1,40 @@
 <template>
-    <div>
-    </div>
+  <div>
+    {{itemId}}
+  </div>
+  <div>
+    {{goodsData.maxGoodsNum}}
+  </div>
+  <div>
+    {{goodsData.useGoodsNum}}
+  </div>
+  <div  v-touch:tap="onChooseGood">返回玩家选择</div>
 </template>
 <style>
 </style>
 <script>
     export default {
-        data () {
-            return {}
-        },
-        components: {}
+      data () {
+          return {
+              choose: 1
+          }
+      },
+      props: [
+        'goodsData'
+      ],
+      computed: {
+          'itemId': function () {
+            return this.goodsData.itemId
+          }
+      },
+      methods: {
+        onChooseGood () {
+          this.$dispatch('ChangeGoods', this.choose)
+        }
+      },
+      components: {
+
+      }
+
     }
 </script>
