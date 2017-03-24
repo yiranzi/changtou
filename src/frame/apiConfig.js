7c6f92ae872b111efdd2a5a153bae5ac494ed07b
FocusOnWrittingBug <506792446@qq.com>
2017-03-24 11:45:28 +0800

    2017.3.24 新手村

--- src/frame/apiConfig.js
+++ src/frame/apiConfig.js
@@ -159,6 +159,7 @@ const apiUrl = {
   'strategy_professional_intro': '/strategy/promotion/professional',
   'strategy_vip_intro': '/strategy/promotion/vip',
   'strategy_notice': '/strategy/data/message/:authority', // 通知
+  'strategy_qq_number': '/strategy/qqGroup/:authority', // qq群号
 
   /**
    * 毕业礼
@@ -221,17 +222,8 @@ const apiUrl = {
   'village_get_answer_record': '/eden/answer-record',  // 获得答题进度
   'village_update_answer_record': '/eden/answer-record/:chapterNo/:questionNo', //提交更新答题进度
   'village_get_proposal_list': '/eden/proposal-list/:pageIndex/:pageSize',  //获得新手村吐槽列表
-  'village_submit_proposal_list': '/eden/proposal-submit',  //提交新手村吐槽
-    /**
-     * 版本信息
-     */
-  'app_update_content': '/system/app/version',            // 获得更新后版本号和更新的功能
-  /**
-   * 头条精选
-   */
-  'homepage_headline': '/homepage/headline',            // 头条精选 内容
-  'homepage_headline_checkin_data': '/homepage/headline/checkin/checkdata',     // 头条精选 签到信息
-  'homepage_headline_checkin_checked': '/homepage/headline/checkin/checked'   // 头条精选 签到功能数据更新
+  'village_submit_proposal_list': '/eden/proposal-submit', //提交新手村吐槽
+  'village_commit_HP': '/eden/HP/:amount'  //提交新手村生命值
 }
 
 export const getUrl = (urlPath, server = SEVER_URL, urlMap = apiUrl) => server + urlMap[urlPath]
